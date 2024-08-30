bf8bda711d69b974bd514f051b7c2193f289675c
Yash Saxena <engineeryashs@gmail.com>
2024-08-30 15:09:08 +0530

    Adding hashrouter instead of browserrouter to do client side routing in my payment app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter,Routes,Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Dashboard } from './pages/Dashboard'
@@ -11,15 +11,14 @@ function App() {
 
   return (
     <>
-    <h1>Hi there</h1>
       <HashRouter>
-      <Routes>
-        <Route path="/signup" element={<Signup/>}></Route>
-        <Route path="/signin" element={<Signin/>}></Route>
-        <Route path="/dashboard" element={<Dashboard/>}></Route> 
-        <Route path="/send" element={<SendMoney/>}></Route> 
-        <Route path="/update" element={<UpdateProfile/>}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/send" element={<SendMoney />}></Route>
+          <Route path="/update" element={<UpdateProfile />}></Route>
+        </Routes>
       </HashRouter>
     </>
   )
