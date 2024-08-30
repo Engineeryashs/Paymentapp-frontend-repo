867ebc68a184e2e3ec15b16e0c0958cf92920787
Yash Saxena <engineeryashs@gmail.com>
2024-08-30 12:28:12 +0530

    Adding updates of hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { HashRouter,Routes,Route } from 'react-router-dom'
 import { Signup } from './pages/Signup'
 import { Signin } from './pages/Signin'
 import { Dashboard } from './pages/Dashboard'
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
     <h1>Hi there</h1>
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
         <Route path="/signup" element={<Signup/>}></Route>
         <Route path="/signin" element={<Signin/>}></Route>
@@ -20,7 +20,7 @@ function App() {
         <Route path="/send" element={<SendMoney/>}></Route> 
         <Route path="/update" element={<UpdateProfile/>}></Route>
       </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
